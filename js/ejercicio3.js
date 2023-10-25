5b3e072655f58ea3e36ee7fca87b35fa19d69871
Natalia <natii.vmorales98@gmail.com>
2023-10-25 02:47:57 -0300

    se agrego el script del ejercicio 3 del tp1

---
+++ js/ejercicio3.js
@@ -0,0 +1,7 @@
+let gradosCelsius = 25;
+
+let gradosFahrenheit = (gradosCelsius * (9/5)) + 32;
+
+console.log("Ejercicio 3");
+console.log("Grados Celsius: " + gradosCelsius);
+console.log("Grados Fahrenheit: " + gradosFahrenheit);
